31af7516b1d79779668c0f049270f8179a8ff2c7
DioAkaTheWorld <burak.ozen7@etu.univ-lorraine.fr>
2026-01-09 16:54:36 +0100

    Exercice 2 ) 1.

--- api_gateway/src/api/routeGateway.php
+++ api_gateway/src/api/routeGateway.php
@@ -1,9 +1,12 @@
 <?php
 
+use gateway\api\actions\DetailPraticienRemoteAction;
 use Slim\App;
 use Gateway\api\actions\ListePraticiensRemoteAction;
 
+
 return function (App $app) {
     $app->get('/praticiens', ListePraticiensRemoteAction::class);
+    $app->get('/praticiens/{id}', DetailPraticienRemoteAction::class);
     return $app;
 };
