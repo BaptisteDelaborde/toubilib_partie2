00340b03ab68444b8f7565ba8e14cea87e6ded50
DioAkaTheWorld <burak.ozen7@etu.univ-lorraine.fr>
2026-01-11 16:57:44 +0100

    Action générique, exercice 2 ) 4.

--- api_gateway/src/api/routeGateway.php
+++ api_gateway/src/api/routeGateway.php
@@ -1,6 +1,7 @@
 <?php
 
 use gateway\api\actions\DetailPraticienRemoteAction;
+use gateway\api\actions\GenericGatewayAction;
 use Slim\App;
 use Gateway\api\actions\ListePraticiensRemoteAction;
 
@@ -8,5 +9,6 @@ use Gateway\api\actions\ListePraticiensRemoteAction;
 return function (App $app) {
     $app->get('/praticiens', ListePraticiensRemoteAction::class);
     $app->get('/praticiens/{id}', DetailPraticienRemoteAction::class);
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', GenericGatewayAction::class);
     return $app;
 };
