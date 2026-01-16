eeaefbc6e768a03751dbaa51a036456295638062
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-01-16 15:11:21 +0100

    /tokens/validate

--- app-auth/config/api.php
+++ app-auth/config/api.php
@@ -1,12 +1,19 @@
 <?php
 
 use toubilib\api\actions\SigninAction;
+use toubilib\api\actions\ValidateTokenAction;
 use toubilib\api\providers\AuthnProviderInterface;
+use toubilib\api\providers\JWTManager;
 
 return [
     SigninAction::class => function($c){
         return new SigninAction(
             $c->get(AuthnProviderInterface::class)
+        );
+    },
+    ValidateTokenAction::class => function($c){
+        return new ValidateTokenAction(
+            $c->get(JWTManager::class)
         );
     },
 ];
--- app-auth/src/api/routes.php
+++ app-auth/src/api/routes.php
@@ -2,11 +2,12 @@
 declare(strict_types=1);
 
 use toubilib\api\actions\SigninAction;
+use toubilib\api\actions\ValidateTokenAction;
 
 return function( \Slim\App $app):\Slim\App {
 
     $app->post('/signin', SigninAction::class)->setName('signin');
-    $app->post('token/validate', ValidateTokenAction::class)->setName('token.validate');
+    $app->post('/tokens/validate', ValidateTokenAction::class)->setName('tokens.validate');
 
     return $app;
 };
